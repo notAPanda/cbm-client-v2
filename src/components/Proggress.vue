cf70785227014d38432bc851ec95f069d873ec4e
Maciej Loch <maqloch@gmail.com>
2022-10-25 08:19:35 +0200

    progress redy

--- src/components/Proggress.vue
+++ src/components/Proggress.vue
@@ -1,2 +1,35 @@
-<template></template>
-<script setup lang="ts"></script>
+<template>
+  <div>
+    <div>
+      <input
+        type="range"
+        min="0"
+        :max="player.durationInSeconds"
+        class="custom-range w-full"
+        v-model="seekInSeconds"
+      />
+    </div>
+    <div>
+      <div class="flex justify-between text-xs text-gray-200">
+        <p>{{ player.seek }}</p>
+        <p>{{ player.duration }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import { computed } from 'vue'
+import _ from 'lodash'
+import { usePlayerStore } from '@/stores/player'
+
+const player = usePlayerStore()
+
+const seekInSeconds = computed({
+  get() {
+    return player.seekInSeconds
+  },
+  set(value) {
+    player.setHowlSeek(value)
+  },
+})
+</script>
